3bf3e4fe237ed037dbe98d575c5f5956b928b430
Savan Sutariya <savanpatel5248@gmail.com>
2024-02-08 10:10:23 +0530

    (feature/ui): homepage ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,12 +5,29 @@ import NotionCard from "../components/common/NotionCard";
 import axios from "axios";
 function HomePage() {
   const [notions, setNotions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchNotions = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("http://localhost:4000/notion");
+        setNotions(response.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchNotions();
+  }, []);
 
   return (
     <>
       <UserLayout>
         <div className="container">
-          {notions.length === 0 ? (
+          {loading && <h2>Loading...</h2>}
+          {error && <h2>Error: {error.message}</h2>}
+          {notions.length === 0 && !loading ? (
             <p>No notions available</p>
           ) : (
             <div className="notion-container">
